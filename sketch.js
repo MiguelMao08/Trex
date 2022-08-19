7aa2d7ad410ce55289a8d290f3267dfb515baf92
MiguelMao08 <109632976+MiguelMao08@users.noreply.github.com>
2022-08-19 19:01:56 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,18 +10,37 @@ var cactoImage4;
 var cactoImage5;
 var cactoImage6;
 var poin= 0;
+var cactos, nuvens;
+var INICIO=0;
+var JOGANDO=1;
+var FIM=2;
+var gameState=INICIO;
+var trex_collided;
+var gameOver, restart;
+var imagemGameOver, imagemRestart;
+var som1, som2, som3;
+var ptero,ptero_fly;
+var pteros;
 //pré-carregamento das imagens e sons
 function preload()
 {
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  groundImage = loadImage("ground2.png")
-  nuvemImage = loadImage("cloud.png")
+  groundImage = loadImage("ground2.png");
+  nuvemImage = loadImage("cloud.png");
   cactoImage1 = loadImage("obstacle1.png");
   cactoImage2 = loadImage("obstacle2.png");
   cactoImage3 = loadImage("obstacle3.png");
   cactoImage4 = loadImage("obstacle4.png");
   cactoImage5 = loadImage("obstacle5.png");
   cactoImage6 = loadImage("obstacle6.png");
+  imagemGameOver = loadImage("gameOver.png");
+  imagemRestart = loadImage("restart.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  som1 = loadSound("jump.mp3");
+  som2 = loadSound("checkPoint.mp3");
+  som3 = loadSound("die.mp3");
+  ptero_fly = loadImage("ptero.png",);
+  
 }
 
 function setup()
@@ -31,7 +50,10 @@ function setup()
   
   //criando o trex
   trex = createSprite(50,160,20,50);
+  trex.setCollider("rectangle",0,0,95,50);
+  trex.debug=false;
   trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided",trex_collided);
 
   //adicione dimensão e posição ao trex
   trex.scale = 0.5;
@@ -48,61 +70,112 @@ function setup()
   //gerando números aleatórios
   var aleatório = Math.round(random(1,61));
   console.log(aleatório);
+  //criando os grupos
+  cactos=new Group ();
+  nuvens=new Group ();
+  pteros=new Group ();
+  //Colocando as imagens restart e gameOver
+  gameOver = createSprite(300,100);
+  gameOver.addImage(imagemGameOver);
+  gameOver.scale =0.5;
+  gameOver.visible=false;
+  restart = createSprite(300,150);
+  restart.addImage(imagemRestart);
+  restart.scale = 0.5;
+  restart.visible=false;
 }
 
 //desenho e animação
 function draw()
-{
-
-  //gerando números aleatórios
-  var aleatório = Math.round(random(1,60));
-  //console.log(aleatório);
-  console.log(frameCount)
-
-
+{ 
   //definir a cor do plano de fundo 
   background("white");
-  
-  //registrando a posição y do trex
-  //console.log(trex.y);
-  
-  //pular quando tecla de espaço for pressionada
-  if(keyDown("space") && trex.y >=160)
+  if(gameState==INICIO)
   {
-    trex.velocityY = -10;
-  }
-  
-  //gravidade para o trex
-  trex.velocityY = trex.velocityY + 0.5;
-
-  //velocidade do solo
-  solo.velocityX = -1
-  //console.log(solo.x);
-
-  //reinicio do solo
-  if(solo.x < 0)
+  text("aperte space para começar",300,100 )
+  if(keyDown("space"))
   {
-    solo.x = width
+  gameState=JOGANDO
+  }
+  }
+  else if (gameState==JOGANDO)
+  {
+   //pular quando tecla de espaço for pressionada
+   if(keyDown("space") && trex.y >=160)
+   {
+     trex.velocityY = -10.5;
+     som1.play();
+    // trex.y = cactos
+   }
+   
+   //gravidade para o trex
+   trex.velocityY = trex.velocityY + 0.3;
+ 
+   //velocidade do solo
+   solo.velocityX = -4-2*poin/100;
+   //console.log(solo.x);
+ 
+   //reinicio do solo
+   if(solo.x < 0)
+   {
+     solo.x = width
+ 
+   }
+   //chamada da função que gera nuvens e cactos
+  gerarNuvens();
+  gerarCactos();
+  //pontuacao
+  poin+=Math.round(getFrameRate()/60);
+  if(cactos.isTouching(trex)||pteros.isTouching(trex))
+  {
+    gameState=FIM
+    som3.play()
+    //trex.velocityY = -10.5;
+  }
+  if (poin%1000 ==0&&poin>0)
+  {
+    som2.play()
+  }
+  //alerar fundo da tela
+  if(poin>=100)
+  {
+    background("black");
+    gerarPteros();
+  }
+  }//jogando
+  else//estado do jogo fim 
+  {
+  cactos.setVelocityXEach(0);
+  nuvens.setVelocityXEach(0);
+  pteros.setVelocityXEach(0);
+  solo.velocityX=0;
+  nuvens.setLifetimeEach(-1);
+  cactos.setLifetimeEach(-1);
+  pteros.setLifetimeEach(-1);
+  trex.changeAnimation("collided",trex_collided);
+  gameOver.visible=true;
+  restart.visible=true;
+  trex.velocityY = 0;
+  if(mousePressedOver(restart))
+  {
+  reset()
+  }
 
   }
+ 
   //impedir que o trex caia
   trex.collide(soloInvisível);
 
-  //chamada da função que gera nuvens
-  gerarNuvens();
-  gerarCactos();
-
   //desenhar os sprites
   drawSprites();
-  //pontuacao
-  poin+=Math.round(frameCount/60)
-  text (" pontos "+poin,50,50)
+  text (" pontos "+poin,50,50);
 }
 
 function gerarNuvens()
 {
   if(frameCount%60==0){
   var nuvem = createSprite(600,50,40,10);
+  nuvens.add(nuvem);
   nuvem.velocityX = -3;
   nuvem.addImage(nuvemImage);
   trex.depth =nuvem.depth +1;
@@ -118,7 +191,8 @@ function gerarCactos()
   if(frameCount%90==0)
   {
     var cacto = createSprite(600,180);
-    cacto.velocityX = -2;
+    cactos.add(cacto)
+    cacto.velocityX = -6-poin/100;
     cacto.scale = 0.5;
     var numero = Math.round(random(1,6));
     switch(numero)
@@ -141,4 +215,25 @@ function gerarCactos()
   }
 }
 
-  
+function reset()
+{
+  gameState=INICIO
+  cactos.destroyEach();
+  nuvens.destroyEach();
+  pteros.destroyEach();
+  poin=0;
+  trex.changeAnimation("running", trex_running);
+  restart.visible = false;
+  gameOver.visible = false;
+}
+function gerarPteros()
+{
+  if(frameCount%100==0)
+  {
+  var ptero = createSprite(600,180);
+  pteros.add(ptero);
+  ptero.velocityX = -6-poin/100;
+  ptero.addImage("ptero",ptero_fly);
+  ptero.lifetime = 310;
+  }
+}  
